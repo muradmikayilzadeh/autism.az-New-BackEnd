8a885b2e18e664c190603861a33a988b910696ee
Murad Mikayilzadeh <Murad Mikayilzadeh>
2017-01-20 18:26:53 +0400

    1/20/2017 18:26

--- app/Http/Controllers/adminController.php
+++ app/Http/Controllers/adminController.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 
 use App\Http\Requests;
 use App\Admin;
-use App\istifadeci;
+use App\Istifadeci;
 class adminController extends Controller
 {
  	public function login(Request $request)
